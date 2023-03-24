623fb99abe25d6f6846336ecd8da9ca04d45b734
stbhai <121119820+stbhai@users.noreply.github.com>
2023-03-24 14:49:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -75,7 +75,7 @@ URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'gplinks.in')
 URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', 'ecd3796d2270b7b8c458112b69e095641c6398e0')
 
      # Auto Delete For Group Message (Self Delete) #
-SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 86400))
+SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 900))
 SELF_DELETE = environ.get('SELF_DELETE', True)
 if SELF_DELETE == "True":
     SELF_DELETE = True
